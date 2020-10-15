01b26e0ec49ab5f7bdb1b0b4e3de8546ea381f9a
nzeal <nshukla@yahoo.com>
2020-10-15 16:57:19 +0200

    Dunder Methods

--- OOP.py
+++ OOP.py
@@ -18,8 +18,14 @@ class BigCircle:
 bg = BigCircle()
 print(bg)
 
+
+'''
+Encapsulation
+'''
+
 # Example-1
 class PlayerCharacter:
+    # Constructor function
     def __init__(self, name, age):  # self refer to Playercharacter 
         self.name = name
         self. age = age
@@ -62,3 +68,42 @@ player2 = PlayerCharacter('Michael', 24)
 print(player1.membership)
 print(player1.name)
 print(player1.shout())
+
+
+# General guidelines 
+
+class NameOfClass():
+    class_attribute = 'value'
+    def __init__(self, param1, param2):
+        self.param1 = param1
+        self. param2 = param2
+
+    def method(self):
+        #code
+    
+    @classmethod
+    def cls_method(cls, param1, param2):
+        #code
+    
+    @staticmethod
+    def stc_method(param1, param2):
+        #code
+
+
+
+'''
+Dunder Methods 
+'''
+
+class Toy():
+    def __init__(self, color, age):
+        self.color = color
+        self.age = age
+        
+    def __str__(self):
+        return f'{self.color}'
+action_figure = Toy('red', 0)
+
+print(action_figure.__str__())
+
+print(str(action_figure))
